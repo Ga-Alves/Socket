dbefa663284c6e8ccaf538740138d84e7e8569b2
Ga-Alves <gabrielalvesreis2gmail.com>
2023-09-23 15:24:57 -0300

    reveal logic

--- common.c
+++ common.c
@@ -13,32 +13,96 @@ perror(msg);
 exit(1);
 }
 
-void HandleTCPServer(int clntSock){
+//------------------//
+// CLIENT FUNCTIONS //
+//------------------//
+void HandleTCPServer(int serverSock){
     int BUFSIZE = sizeof(struct message);
     struct message message;
     memset(&message, 0, sizeof message);
 
-    // init game
-    message.type = 0;
-    ssize_t numBytesSent = send(clntSock, &message, sizeof(message), 0);
-    if (numBytesSent < 0)
-        DieWithSystemMessage("send() failed");
+    char userCommand[11] = {};
 
-    do{
-        ssize_t numBytesRcvd = recv(clntSock, &message, BUFSIZE, 0);
+    scanf("%s", userCommand);
+    int start = !strcmp(userCommand, "start");
+    if (start){
+        // init game
+        message.type = START_TYPE;
+        ssize_t numBytesSent = send(serverSock, &message, sizeof(message), 0);
+        if (numBytesSent < 0)
+            DieWithSystemMessage("send() failed");
+        
+        
+        ssize_t numBytesRcvd = recv(serverSock, &message, BUFSIZE, 0);
         if (numBytesRcvd < 0)
             DieWithSystemMessage("recv() failed");
-        
+
         printBoard(message);
-    }while(0);
+    }
+    else {
+        DieWithSystemMessage("you should start the game first!");
+    }
+
+
+    do{
+        char userCommand[11] = {};
+        int x = -1;
+        int y = -1;
+
+        scanf("%s %d, %d", userCommand, &x, &y );
+
+        int start = !strcmp(userCommand, "start");
+        int reveal = !strcmp(userCommand, "reveal");
+        // int flag = !strcmp(userCommand, "flag");
+        // int remove_flag = !strcmp(userCommand, "remove_flag");
+        // int reset = !strcmp(userCommand, "reset");
+        int exit = !strcmp(userCommand, "exit");
+
+        if (reveal){
+            message.type = REVEAL_TYPE;
+            message.coordinates[0] = x;
+            message.coordinates[1] = y;
+        }
+
+        ssize_t numBytesSent = send(serverSock, &message, sizeof(message), 0);
+        if (numBytesSent < 0)
+            DieWithSystemMessage("send() failed");
+        
+        
+        ssize_t numBytesRcvd = recv(serverSock, &message, BUFSIZE, 0);
+        if (numBytesRcvd < 0)
+            DieWithSystemMessage("recv() failed");
+
+        printBoard(message);
+    }while(1);
 }
 
 void printBoard(struct message message){
     for (int i = 0; i < 4; i++){
-        printf("%d, %d, %d, %d\n", message.board[i][0], message.board[i][1], message.board[i][2], message.board[i][3] );
+        for (int j = 0; j < 4; j++){
+            switch (message.board[i][j])
+            {
+            case -1:
+                printf("* ");
+                break;
+            case -2:
+                printf("- ");
+                break;
+            case -3:
+                printf("> ");
+                break;
+            default:
+                printf("%d ", message.board[i][j]);
+                break;
+            }
+        }
+        printf("\n");
     };
 }
 
+//------------------//
+// SERVER FUNCTIONS //
+//------------------//
 void HandleTCPClient(int clntSock, const char* gamePath){
 
     int BUFSIZE = sizeof(struct message);
@@ -50,51 +114,39 @@ void HandleTCPClient(int clntSock, const char* gamePath){
     if (fileResponse < 0)
         DieWithSystemMessage("open(<input_file>) failed\n");
 
-    // waiting for init game request
-    int numBytesRcvd = recv(clntSock, &userGameBoard, BUFSIZE, 0);
-    if (numBytesRcvd < 0)
-        DieWithSystemMessage("recv() failed");
-    if(userGameBoard.type != 0)
-        DieWithSystemMessage("player should start game");
-    else{
-        // send board to play
-        memset(&userGameBoard.board, -2, sizeof(userGameBoard.board));
-
-        for (int i = 0; i < 4; i++){
-            userGameBoard.board[i][0] = -2;
-            userGameBoard.board[i][1] = -2;
-            userGameBoard.board[i][2] = -2;
-            userGameBoard.board[i][3] = -2;
-        };
-
-        ssize_t numBytesSent = send(clntSock, &userGameBoard, sizeof(userGameBoard), 0);
-        if (numBytesSent < 0)
-            DieWithSystemMessage("send() failed");
-    }
+    
     
    do { 
         // See if there is more data to receive
-        int numBytesRcvd = recv(clntSock, &userGameBoard, BUFSIZE, 0);
+        printf("-------h-----------\n");
+      /**/  int numBytesRcvd = recv(clntSock, &userGameBoard, BUFSIZE, 0);
         if (numBytesRcvd < 0)
             DieWithSystemMessage("recv() failed");
 
-        printf("useAction: %d\n", userGameBoard.type);
-        
-        // switch (userGameBoard.type){
-        //     case 0:
-                
-        //         break;
-            
-        //     default:
-        //     break;
-        // }
-        
-        // ssize_t numBytesSent = send(clntSock, &serverGameBoard, sizeof(serverGameBoard), 0);
-        // if (numBytesSent < 0)
-        //     DieWithSystemMessage("send() failed");
+        if (userGameBoard.type == START_TYPE){
+            // preparing board to play
+            memset(&userGameBoard, 0, sizeof(userGameBoard));
+            for (int i = 0; i < 4; i++){
+                userGameBoard.board[i][0] = -2;
+                userGameBoard.board[i][1] = -2;
+                userGameBoard.board[i][2] = -2;
+                userGameBoard.board[i][3] = -2;
+            };
 
+        }
+        else if (userGameBoard.type == REVEAL_TYPE){
+            int x = userGameBoard.coordinates[0];
+            int y = userGameBoard.coordinates[1];
+            printf("cooordenaadas: %d %d\n", x, y);
+            userGameBoard.board[x][y] = serverGameBoard.board[x][y];
+        }
+
+        // send board to play
+        ssize_t numBytesSent = send(clntSock, &userGameBoard, sizeof(userGameBoard), 0);
+        if (numBytesSent < 0)
+            DieWithSystemMessage("send() failed");
         
-    }  while (0);
+    }  while (1);
     
 
 }
@@ -104,9 +156,9 @@ int initGameBoard(struct message *serverGameBoard, const char* gamePath){
     if (gameConfig == NULL){
         return -1;
     }
-    int read[4][4] = {};
+
     for (int i = 0; i < 4; i++){
-        fscanf(gameConfig, "%d, %d, %d, %d", &read[i][0], &read[i][1], &read[i][2], &read[i][3] );
+        fscanf(gameConfig, "%d, %d, %d, %d", &serverGameBoard->board[i][0], &serverGameBoard->board[i][1], &serverGameBoard->board[i][2], &serverGameBoard->board[i][3] );
     };
     fclose(gameConfig);
 }
--- common.h
+++ common.h
@@ -6,6 +6,16 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
+#define START_TYPE 0
+#define REVEAL_TYPE 1
+#define FLAG_TYPE 2
+#define STATE_TYPE 3
+#define REMOVE_FLAG_TYPE 4
+#define RESET_TYPE 5
+#define WIN_TYPE 6
+#define EXIT_TYPE 7
+#define GAME_OVER_TYPE 8
+
 struct message {
     int type;
     int coordinates[2];
@@ -13,7 +23,11 @@ struct message {
 };
 void DieWithUserMessage(const char *msg, const char *detail) ;
 void DieWithSystemMessage(const char *msg);
-void HandleTCPServer(int clntSock);
+
+//server functions
 void HandleTCPClient(int clntSock, const char* gamePath);
 int initGameBoard(struct message *serverGameBoard, const char* gamePath);
+
+//client functions
+void HandleTCPServer(int serverSock);
 void printBoard(struct message message);
